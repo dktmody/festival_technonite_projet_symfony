687404f846f089b1da1bcbbe678d728c238df104
mody <diakhite.mody94@gmail.com>
2021-06-21 12:53:47 +0200

    creat Technocontroller

---
+++ src/Controller/TechnoController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TechnoController extends AbstractController
+{
+    /**
+     * @Route("/", name="techno_home")
+     */
+    public function home(): Response
+    {
+        return $this->render('techno/home.html.twig', [
+            'controller_name' => 'TechnoController',
+        ]);
+    }
+}
